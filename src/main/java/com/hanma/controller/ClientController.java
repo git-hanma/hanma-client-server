7eeca1e0cde0e599154eb8172dd21e52b6162eae
shenxiaoyan <279700467@qq.com>
2019-07-15 18:00:34 +0800

    前台客户端提交注册页面

--- src/main/java/com/hanma/controller/ClientController.java
+++ src/main/java/com/hanma/controller/ClientController.java
@@ -16,14 +16,46 @@ import javax.websocket.server.PathParam;
  */
 @Controller
 public class ClientController {
+    /**
+    *@Description: ClientController java类作用描述:首页
+    *@Param: * @param null
+    *@return: 
+    *@Author: 申笑琰
+    *@CreateDate: 2019/7/15 下午 05:39
+    */
     @RequestMapping("/index")
     public String gotoIndex(){
         return "index";
     }
+    /**
+    *@Description: ClientController java类作用描述:登录
+    *@Param: * @param null
+    *@return: 
+    *@Author: 申笑琰
+    *@CreateDate: 2019/7/15 下午 05:39
+    */
     @RequestMapping("/login")
     public String gotoLogin(){
         return "login";
     }
+    /**
+    *@Description: ClientController java类作用描述:注册
+    *@Param: * @param null
+    *@return: 
+    *@Author: 申笑琰
+    *@CreateDate: 2019/7/15 下午 05:40
+    */
+    @RequestMapping("/register")
+    public String gotoRegister(){
+        return "register";
+    }
+    /**
+    *@Description: ClientController java类作用描述:根据path跳转
+    *@Param: * @param null
+    *@return: 
+    *@Author: 申笑琰
+    *@CreateDate: 2019/7/15 下午 05:40
+    */
     @RequestMapping("path")
     public String fotoPath(String path){
         return path;
